091b6f405e923e0f5698c925a52ee338a66d4a84
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-04-29 21:38:49 -0400

    Receiving message works, testing needs to be done

--- receiving_test/android/app/src/main/kotlin/com/example/receiving_test/SmsReceiver.kt
+++ receiving_test/android/app/src/main/kotlin/com/example/receiving_test/SmsReceiver.kt
@@ -1,57 +1,57 @@
-package com.example.app_name
+package com.example.receiving_test
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.Build
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import android.provider.Telephony
+import androidx.core.app.NotificationCompat
+import android.telephony.SmsMessage
+import android.util.Log
 
 class SmsReceiver : BroadcastReceiver() {
-    private val NOTIFICATION_ID = 1
-    private val CHANNEL_ID = "SMSReceiverChannel"
 
-    override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent?.action == Telephony.Sms.Intents.SMS_RECEIVED_ACTION) {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Telephony.Sms.Intents.SMS_DELIVER_ACTION) {
             val smsMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
-            smsMessages.forEach { message ->
-                // Process the message here
-                showNotification(context, message.originatingAddress ?: "Unknown", message.messageBody)
+            for (message in smsMessages) {
+                showNotification(context, message)
             }
         }
     }
 
-    private fun showNotification(context: Context?, sender: String, message: String) {
-        createNotificationChannel(context)
+    private fun showNotification(context: Context, message: SmsMessage) {
+        val channelId = "sms_channel_id"
+        val notificationId = 101
 
-        val builder = NotificationCompat.Builder(context!!, CHANNEL_ID)
-            .setSmallIcon(R.drawable.notification_icon)
-            .setContentTitle("New SMS from $sender")
-            .setContentText(message)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-
-        with(NotificationManagerCompat.from(context)) {
-            notify(NOTIFICATION_ID, builder.build())
-        }
-    }
-
-    private fun createNotificationChannel(context: Context?) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "SMS Receiver Channel"
-            val descriptionText = "Notification channel for SMS Receiver"
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-                enableLights(true)
-                lightColor = Color.RED
-            }
-            val notificationManager: NotificationManager =
-                context!!.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
+            val channel = NotificationChannel(
+                channelId,
+                "SMS Notifications",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            val manager = context.getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
         }
+
+        val notificationIntent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0)
+
+        val notification = NotificationCompat.Builder(context, channelId)
+            .setContentTitle("New SMS received")
+            .setContentText("From: ${message.originatingAddress}, Message: ${message.messageBody}")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        val notificationManager = context.getSystemService(NotificationManager::class.java)
+        notificationManager.notify(notificationId, notification)
     }
 }
--- receiving_test/lib/main.dart
+++ receiving_test/lib/main.dart
@@ -1,65 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MySmsApp());
 }
 
-class MyApp extends StatelessWidget {
+class MySmsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter SMS Receiver',
+      title: 'My SMS App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter SMS Receiver'),
+      home: MyHomePage(title: 'My SMS App Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  static const platform = const MethodChannel("com.example.chat/chat");
-
-  @override
-  void initState() {
-    super.initState();
-    setDefaultSmsApp();
-  }
-
-  Future<void> setDefaultSmsApp() async {
-    try {
-      final result = await platform.invokeMethod('setDefaultSms');
-      print("Result: $result");
-    } on PlatformException catch (e) {
-      print("Error: $e");
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'SMS Receiver is running.',
-            ),
-          ],
+        child: Text(
+          'Welcome to My SMS App!',
         ),
       ),
     );
