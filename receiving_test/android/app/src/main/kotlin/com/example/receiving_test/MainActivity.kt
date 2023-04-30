387bc2d5d08deb870033a16902777d4061311b70
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-04-30 11:13:13 -0400

    Sending sms works

--- receiving_test/android/app/src/main/kotlin/com/example/receiving_test/MainActivity.kt
+++ receiving_test/android/app/src/main/kotlin/com/example/receiving_test/MainActivity.kt
@@ -1,6 +1,41 @@
 package com.example.receiving_test
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+import android.telephony.SmsManager
+import android.widget.Toast
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.receiving_test"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "sendSms") {
+                val num = call.argument<String>("phoneNumber")
+                val msg = call.argument<String>("message")
+                if (num != null && msg != null) {
+                    sendSms(num, msg)
+                    result.success("SMS Sent")
+                } else {
+                    result.error("UNAVAILABLE", "SMS not sent", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun sendSms(phoneNumber: String, message: String) {
+        try {
+            val smsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage(phoneNumber, null, message, null, null)
+            Toast.makeText(this, "SMS sent.", Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            Toast.makeText(this, "SMS failed, please try again.", Toast.LENGTH_LONG).show()
+            e.printStackTrace()
+        }
+    }
 }
--- receiving_test/lib/main.dart
+++ receiving_test/lib/main.dart
@@ -1,38 +1,85 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
+  import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MySmsApp());
-}
-
-class MySmsApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My SMS App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'My SMS App Home Page'),
-    );
+  void main() {
+    runApp(MyApp());
   }
-}
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Welcome to My SMS App!',
+  class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'SMS Sender',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-    );
+        home: SmsSender(),
+      );
+    }
   }
-}
+
+  class SmsSender extends StatefulWidget {
+    @override
+    _SmsSenderState createState() => _SmsSenderState();
+  }
+
+  class _SmsSenderState extends State<SmsSender> {
+    final _phoneNumberController = TextEditingController();
+    final _messageController = TextEditingController();
+
+    Future<void> _sendSms() async {
+      try {
+        const platform = MethodChannel('com.example.receiving_test');
+        final String phoneNumber = _phoneNumberController.text;
+        final String message = _messageController.text;
+        await platform.invokeMethod('sendSms', <String, dynamic>{
+          'phoneNumber': phoneNumber,
+          'message': message,
+        });
+      } on PlatformException catch (e) {
+        print('Failed to send SMS: ${e.message}');
+      }
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('SMS Sender'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: _phoneNumberController,
+                decoration: InputDecoration(
+                  labelText: 'Phone number',
+                ),
+                keyboardType: TextInputType.phone,
+              ),
+              TextField(
+                controller: _messageController,
+                decoration: InputDecoration(
+                  labelText: 'Message',
+                ),
+                maxLines: null,
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                child: Text('Send SMS'),
+                onPressed: _sendSms,
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    @override
+    void dispose() {
+      _phoneNumberController.dispose();
+      _messageController.dispose();
+      super.dispose();
+    }
+  }
