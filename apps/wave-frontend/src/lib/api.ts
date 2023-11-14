c564ecfa82975091d88b41737ac1f9cf978abecc
Laurent Stéphenne <laurent@guibi.ca>
2023-11-13 23:34:29 -0500

    Websocket dont work?

--- apps/wave-frontend/src/lib/api.ts
+++ apps/wave-frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import { edenTreaty } from "@elysiajs/eden";
 import type { App } from "wave-backend";
 
-const api = edenTreaty<App>("http://0.0.0.0:3000");
+const api = edenTreaty<App>("http://localhost:3000");
 
 export default api;
