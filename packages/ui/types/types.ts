f70620776ee5966544a3b3c1c057cc655f75e3e9
azomDev <azom.developer@gmail.com>
2023-11-20 17:19:27 -0500

    refactor_function_names_and_variable_names

--- packages/ui/types/types.ts
+++ packages/ui/types/types.ts
@@ -10,10 +10,10 @@ export type Message = {
     text: string;
 };
 
-export const isConversation = (data: any): data is Conversation => {
+export const is_conversation = (data: any): data is Conversation => {
     return typeof data === "object" && typeof data.name === "string" && typeof data.id === "number";
 };
 
-export const isMessage = (data: any): data is Message => {
+export const is_message = (data: any): data is Message => {
     return typeof data === "object" && typeof data.id === "number" && typeof data.text === "string";
 };
