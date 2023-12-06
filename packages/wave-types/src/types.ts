feb1af5e1db6c57bf5530b583d0bf291d303d3f8
Laurent Stéphenne <laurent@guibi.ca>
2023-12-05 20:34:42 -0500

    Types are now in the proper package

--- packages/wave-types/src/types.ts
+++ packages/wave-types/src/types.ts
@@ -1,14 +1,14 @@
-export type Conversation = {
+export interface Conversation {
     name: string;
     id: number;
-};
+}
 
-export type Message = {
+export interface Message {
     // sender: string;
     // sent_time_stamp: string;
     id: number;
     text: string;
-};
+}
 
 export const is_conversation = (data: any): data is Conversation => {
     return typeof data === "object" && typeof data.name === "string" && typeof data.id === "number";
