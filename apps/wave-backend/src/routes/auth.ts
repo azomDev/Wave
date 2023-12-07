4504707077ad0e49230b4db0c3eb99f86563f4e3
Laurent Stéphenne <laurent@guibi.ca>
2023-12-07 15:38:29 -0500

    Small bugfix

--- apps/wave-backend/src/routes/auth.ts
+++ apps/wave-backend/src/routes/auth.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 
-export const authRoutes = () =>
-    new Elysia().group("/auth", (app) =>
+export const authRoutes = (app: Elysia) =>
+    app.group("/auth", (app) =>
         app.post("/login", () => {
             console.log("login");
         })
