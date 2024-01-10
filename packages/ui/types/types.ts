642d03a9fcd75df0f573fa0ca5584756408e601e
Laurent Stéphenne <guibi.ss999@gmail.com>
2024-01-10 16:43:39 -0500

    Revert "Types are now in the proper package"
    This reverts commit feb1af5e1db6c57bf5530b583d0bf291d303d3f8.

--- packages/ui/types/types.ts
+++ packages/ui/types/types.ts
@@ -1,14 +1,14 @@
-export interface Conversation {
+export type Conversation = {
     name: string;
     id: number;
-}
+};
 
-export interface Message {
+export type Message = {
     // sender: string;
     // sent_time_stamp: string;
     id: number;
     text: string;
-}
+};
 
 export const is_conversation = (data: any): data is Conversation => {
     return typeof data === "object" && typeof data.name === "string" && typeof data.id === "number";
