e101740a4d96b995079241154128930197077f43
Laurent Stéphenne <guibi.ss999@gmail.com>
2024-01-10 16:43:39 -0500

    Revert "added sveltekit"
    This reverts commit fd82dda0896b143a3ddb058b113c4d380843890e.

--- apps/wave-frontend/src/lib/api.ts
+++
@@ -1,6 +0,0 @@
-import { edenTreaty } from "@elysiajs/eden";
-import type { App } from "wave-backend";
-
-const api = edenTreaty<App>("http://0.0.0.0:3000");
-
-export default api;
--- apps/wave-frontend/vite.config.ts
+++ apps/wave-frontend/vite.config.ts
@@ -1,6 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  plugins: [svelte()],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+  }
+}));
