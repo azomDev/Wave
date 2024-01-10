c39c6551c703daa6aa0c81b7ffc35f0b56f4e35b
Laurent Stéphenne <guibi.ss999@gmail.com>
2024-01-10 16:43:39 -0500

    Revert "Elysia websocket works!"
    This reverts commit 0441524030ae8229ff0311523f35c3f020fcda5d.

--- apps/wave-backend/src/index.ts
+++ apps/wave-backend/src/index.ts
@@ -2,25 +2,24 @@ import { Elysia, t } from "elysia";
 
 const app = new Elysia()
     .get("/", () => "Hello Elysia")
-    .ws("/chat/:chatId", {
-        params: t.Object({ chatId: t.String({ minLength: 20, maxLength: 20 }) }),
+    .ws("/client", {
         body: t.Object({
             username: t.String(),
             message: t.String(),
         }),
         open(ws) {
-            const msg = `${ws.remoteAddress} has entered the chat`;
+            const msg = `${ws.data.body.username} has entered the chat`;
             ws.subscribe("the-group-chat");
             ws.publish("the-group-chat", { message: msg });
         },
         message(ws, message) {
-            console.log("Received", message.message, "from", message.username);
+            console.log("Received", message.username, "from", message.username);
             ws.send("The message has been sent to the group.");
             // the server re-broadcasts incoming messages to everyone
             ws.publish("the-group-chat", message);
         },
         close(ws) {
-            const msg = `${ws.remoteAddress} has left the chat`;
+            const msg = `${ws.data.body.username} has left the chat`;
             ws.publish("the-group-chat", { message: msg });
             ws.unsubscribe("the-group-chat");
         },
